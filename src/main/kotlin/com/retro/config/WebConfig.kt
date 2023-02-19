68da0dbf5b37cd8b65d9d7e3c6cb0f6dbe335f1a
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-02-19 23:54:37 +0900

    Cors 설정

--- src/main/kotlin/com/retro/config/WebConfig.kt
+++ src/main/kotlin/com/retro/config/WebConfig.kt
@@ -3,6 +3,7 @@ package com.retro.config
 import com.retro.common.MemberAttributeResolver
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
+import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
@@ -11,5 +12,10 @@ class WebConfig(
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(memberAttributeResolver)
+    }
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins("https://retromall.herokuapp.com")
     }
 }
