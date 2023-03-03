4b5a2c4ea1691d01163dd070a3aa7c719dae92bc
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-03-03 11:54:08 +0900

    QueryExceptionHandler추가

--- src/main/kotlin/com/retro/common/QueryExceptionHandler.kt
+++ src/main/kotlin/com/retro/common/QueryExceptionHandler.kt
@@ -1,13 +1,19 @@
 package com.retro.common
 
+import org.hibernate.QueryException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
 import java.sql.SQLIntegrityConstraintViolationException
 
 @ControllerAdvice
-class SqlExceptionHandler {
+class QueryExceptionHandler {
     @ExceptionHandler(SQLIntegrityConstraintViolationException::class)
     fun sqlIntegrityConstraintViolationException() {
+        return
+    }
+
+    @ExceptionHandler(QueryException::class)
+    fun queryException() {
         return
     }
 }
