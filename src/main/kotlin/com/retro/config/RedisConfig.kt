6c1b3d8e31fab0d8ac20f26a3e1f6d446420d650
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-03-08 20:17:14 +0900

    no message

--- src/main/kotlin/com/retro/config/RedisConfig.kt
+++ src/main/kotlin/com/retro/config/RedisConfig.kt
@@ -3,12 +3,18 @@ package com.retro.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
 
 @Configuration
 class RedisConfig {
+    @Bean
+    fun redisConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(RedisStandaloneConfiguration("localhost", 6379))
+    }
     @Bean
     fun redisTemplate(connectionFactory: RedisConnectionFactory): RedisTemplate<String, Any> {
         val template = RedisTemplate<String, Any>()
