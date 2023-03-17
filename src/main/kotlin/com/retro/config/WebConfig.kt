a90040d682baee98c51fcab74befb1cb2211cf61
dlwlgh9412 <dlwlgh9412@gmail.com>
2023-03-17 09:30:08 +0900

    cors 정책 로직 변경

--- src/main/kotlin/com/retro/config/WebConfig.kt
+++ src/main/kotlin/com/retro/config/WebConfig.kt
@@ -2,20 +2,23 @@ package com.retro.config
 
 import com.retro.common.MemberAttributeResolver
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig(
-    private val memberAttributeResolver: MemberAttributeResolver
+    private val memberAttributeResolver: MemberAttributeResolver,
+    private val environment: Environment
 ) : WebMvcConfigurer {
     override fun addArgumentResolvers(resolvers: MutableList<HandlerMethodArgumentResolver>) {
         resolvers.add(memberAttributeResolver)
     }
 
     override fun addCorsMappings(registry: CorsRegistry) {
-        if (System.getProperty("spring.profiles.active") == "local") {
+        val activeProfiles = environment.activeProfiles
+        if (activeProfiles.contains("local")) {
             registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PATCH", "DELETE")
@@ -25,6 +28,10 @@ class WebConfig(
         } else {
             registry.addMapping("/**")
                 .allowedOrigins("https://retromall.herokuapp.com")
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600)
         }
     }
 }
