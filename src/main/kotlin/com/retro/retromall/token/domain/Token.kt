609bc5cb4ade37f36734b3292a217374dcda32b2
tlatmsrud <tlatmsrud@naver.com>
2023-04-06 22:33:52 +0900

    Token 도메인 생성

--- src/main/kotlin/com/retro/retromall/token/domain/Token.kt
+++ src/main/kotlin/com/retro/retromall/token/domain/Token.kt
@@ -1,4 +1,29 @@
 package com.retro.retromall.token.domain
 
-class Token {
+import com.retro.retromall.member.domain.Member
+import java.time.LocalDateTime
+import javax.persistence.*
+
+@Entity
+@Table(name="tb_token")
+class Token (
+    member : Member,
+    refreshToken : String
+){
+
+    @Id
+    @Column(name = "token_id")
+    val id : Long? = null
+
+    @OneToOne
+    @JoinColumn(name="member_id")
+    var member : Member = member
+
+    @Column(name = "refresh_token")
+    var refreshToken :String = refreshToken
+
+    fun updateRefreshToken(refreshToken: String){
+        this.refreshToken = refreshToken
+    }
+
 }
