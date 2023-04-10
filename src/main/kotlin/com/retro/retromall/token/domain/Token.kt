b1b5e9871ba8a9d758a6897d52fe6fe7ff8fee6b
tlatmsrud <tlatmsrud@naver.com>
2023-04-10 13:24:21 +0900

    JavaDoc 생성을 위한 주석 추가 및 코드 리펙토링

--- src/main/kotlin/com/retro/retromall/token/domain/Token.kt
+++ src/main/kotlin/com/retro/retromall/token/domain/Token.kt
@@ -4,6 +4,10 @@ import com.retro.retromall.member.domain.Member
 import javax.persistence.*
 
 
+/**
+ * Domain For Token
+ * @author sim
+ */
 @Entity
 @Table(name="tb_token")
 class Token (
@@ -23,6 +27,11 @@ class Token (
     @Column(name = "refresh_token")
     var refreshToken :String = refreshToken
 
+    /**
+     * 리프레시 토큰 수정
+     * @author sim
+     * @param refreshToken - 리프레시 토큰
+     */
     fun updateRefreshToken(refreshToken: String){
         this.refreshToken = refreshToken
     }
