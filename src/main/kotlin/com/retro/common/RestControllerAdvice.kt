b66541cf1e8081ec79f7f6a2670fc402818b4e51
tlatmsrud <tlatmsrud@naver.com>
2023-04-18 19:17:25 +0900

    예외 핸들러 메서드 추가
    1. MethodArgumentNotValidException (Valid로 발생한 예외)
    2. ConstraintViolationException (Validated로 발생한 예외)

--- src/main/kotlin/com/retro/common/RestControllerAdvice.kt
+++ src/main/kotlin/com/retro/common/RestControllerAdvice.kt
@@ -5,8 +5,11 @@ import io.jsonwebtoken.MalformedJwtException
 import io.jsonwebtoken.UnsupportedJwtException
 import io.jsonwebtoken.security.SecurityException
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import javax.validation.ConstraintViolationException
+
 
 @ControllerAdvice
 class RestControllerAdvice {
@@ -38,6 +41,19 @@ class RestControllerAdvice {
     @ExceptionHandler(UnsupportedJwtException::class)
     fun unsupportedJwtException(e: UnsupportedJwtException): ResponseEntity<Any> {
         return ResponseEntity.ok(ErrorResponse(e.message))
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun methodArgumentNotValidException(e : MethodArgumentNotValidException) : ResponseEntity<Any>{
+        val message = e.bindingResult.allErrors.stream().findFirst()
+            .get().defaultMessage
+        return ResponseEntity.ok(ErrorResponse(message))
+    }
+    @ExceptionHandler(ConstraintViolationException::class)
+    fun constraintViolationException(e: ConstraintViolationException): ResponseEntity<Any?>? {
+        val message = e.constraintViolations.stream().findFirst()
+            .get().message
+        return ResponseEntity.ok(ErrorResponse(message))
     }
 
     data class ErrorResponse(
