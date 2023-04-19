85087b2ce15a5fe323fa68da6842bfeb32bd4f5c
tlatmsrud <tlatmsrud@naver.com>
2023-04-20 02:59:14 +0900

    토큰 도메인 수정
    기존 ID 제거 후 memberId로 대체.

--- src/main/kotlin/com/retro/retromall/token/domain/Token.kt
+++ src/main/kotlin/com/retro/retromall/token/domain/Token.kt
@@ -1,6 +1,6 @@
 package com.retro.retromall.token.domain
 
-import com.retro.retromall.member.domain.Member
+
 import javax.persistence.*
 
 /**
@@ -10,19 +10,14 @@ import javax.persistence.*
 @Entity
 @Table(name="tb_token")
 class Token (
-    member : Member,
+    memberId : Long,
     refreshToken : String,
     expirationRefreshToken: Long
-){
+) {
+
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "token_id")
-    var id : Long? = null
-
-    @OneToOne
-    @JoinColumn(name="member_id")
-    var member : Member = member
+    var memberId : Long = memberId
 
     @Column(name = "refresh_token")
     var refreshToken :String = refreshToken
@@ -38,5 +33,6 @@ class Token (
     fun updateRefreshToken(refreshToken: String){
         this.refreshToken = refreshToken
     }
+
 
 }
