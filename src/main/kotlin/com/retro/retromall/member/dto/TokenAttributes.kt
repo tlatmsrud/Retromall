47d9d54cb3a831645c77c44bac8adeb0a7faec43
tlatmsrud <tlatmsrud@naver.com>
2023-04-20 13:56:58 +0900

    TokenAttributes에 expiredRefreshToken 제거
    reason : 클라이언트가 받을 필요가 없는 필드로 판단

--- src/main/kotlin/com/retro/retromall/member/dto/TokenAttributes.kt
+++ src/main/kotlin/com/retro/retromall/member/dto/TokenAttributes.kt
@@ -3,6 +3,5 @@ package com.retro.retromall.member.dto
 data class TokenAttributes(
     var grantType: String? = null,
     var accessToken: String? = null,
-    var expiredAccessToken: Long? = null,
-    var expiredRefreshToken: Long? = null
+    var expiredAccessToken: Long? = null
 )
