3fdbc04c2a1acfe2ab9438d6b51aee6b80ff00ec
tlatmsrud <tlatmsrud@naver.com>
2023-06-16 17:57:41 +0900

    :art: Update-AutocompleteService
    1. 검색 횟수 기준 내림차순으로 자동완성되도록 수정
    2. 자동완성 단어 추가 메서드 생성

--- src/main/kotlin/com/retro/retromall/autocomplete/service/AutocompleteService.kt
+++ src/main/kotlin/com/retro/retromall/autocomplete/service/AutocompleteService.kt
@@ -13,22 +13,57 @@ class AutocompleteService (
     private val redisTemplate : RedisTemplate<String, String>,
     @Value("\${autocomplete.limit}") private val limit: Long,
     @Value("\${autocomplete.suffix}") private val suffix : String,
-    @Value("\${autocomplete.key}") private val key : String
+    @Value("\${autocomplete.key}") private val key : String,
+    @Value("\${autocomplete.score-key}") private val scoreKey : String
 ){
     fun getAutocomplete(searchWord : String) : AutocompleteResponse {
+
+        val autocompleteList = getAutoCompleteListFromRedis(searchWord)
+        return sortAutocompleteListByScore(autocompleteList)
+    }
+
+    fun addAutocomplete(searchWord : String ){
+
         val zSetOperations = redisTemplate.opsForZSet()
-        var autoCompleteList  = emptyList<String>()
+        zSetOperations.incrementScore(scoreKey, searchWord, 1.0)
+
+        zSetOperations.score(key, searchWord)?:let {
+            for(i in 1..searchWord.length){
+                zSetOperations.add(key, searchWord.substring(0,i),0.0)
+            }
+            zSetOperations.add(key, searchWord+suffix,0.0)
+        }
+
+    }
+
+    fun getAutoCompleteListFromRedis(searchWord : String) : List<String> {
+
+        val zSetOperations = redisTemplate.opsForZSet()
+        var autocompleteList = emptyList<String>()
 
         zSetOperations.rank(key, searchWord)?.let {
-
-            val rangeList = zSetOperations.range(key, it, -1) as Set<String>
-
-            autoCompleteList =  rangeList.stream()
-                .filter { value -> value.endsWith(suffix) && value.startsWith(searchWord)}
-                .map { value -> StringUtils.removeEnd(value,suffix) }
+            val rangeList = zSetOperations.range(key, it, it + 1000) as Set<String>
+            autocompleteList = rangeList.stream()
+                .filter { value -> value.endsWith(suffix) && value.startsWith(searchWord) }
+                .map { value -> StringUtils.removeEnd(value, suffix) }
                 .limit(limit)
                 .toList()
         }
-        return AutocompleteResponse(autoCompleteList)
+
+        return autocompleteList
+    }
+
+    fun sortAutocompleteListByScore(autocompleteList : List<String>) : AutocompleteResponse{
+        val zSetOperations = redisTemplate.opsForZSet()
+
+        val list = arrayListOf<AutocompleteResponse.Data>()
+        autocompleteList.forEach{word ->
+                zSetOperations.score(scoreKey, word)?.let {
+                    list.add(AutocompleteResponse.Data(word, it))
+                }
+        }
+        list.sortByDescending { it.score }
+        return AutocompleteResponse(list)
+
     }
 }
